1b08fc2da30b44094ce903ddd67b3f7db62cdbac
nikosRepo <nikosrep@gmail.com>
2019-08-02 18:31:47 +0300

    Face Rec Back-End part

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "b02c44dc56e8436da33a44e92eff0da5"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with Api"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+};
