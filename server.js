596e587791aeaa2c4ea6ea4a404beb1d780ad699
nikosRepo <nikosrep@gmail.com>
2019-08-03 13:36:05 +0300

    database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-encircled-73329",
-    user: "postgres",
-    password: "admin",
-    database: "smart-brain"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
